8c05f7f3a4bd0fd7b81fb2c8a106cae5718edad1
Ryan Dowler <ryandowler99@hotmail.com>
2018-08-05 00:02:29 +0100

    ignore

--- config/application.rb
+++ config/application.rb
@@ -20,11 +20,15 @@ module MyMoneyCounts1
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+
+    config.assets.initialize_on_precompile = false
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_controller.include_all_helpers = false
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
 
 
   end
