6f5df439276c69c71ec0c15998402e489b99fe60
Ryan Dowler <ryandowler99@hotmail.com>
2018-10-17 11:11:32 +0100

    assistant features added

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -69,6 +69,6 @@ class VideosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def video_params
-      params.require(:video).permit(:title, :description, :link, :icon)
+      params.require(:video).permit(:title, :description, :link, :icon, :category)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916144319) do
+ActiveRecord::Schema.define(version: 20181015151733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20180916144319) do
     t.string   "icon"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "category"
   end
 
   add_foreign_key "budgets", "users"
